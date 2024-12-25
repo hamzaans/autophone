c823d3b63bd7ad75caa0a026b06ba3381ddd279d
hamza <hamza@hansari.org>
2024-12-25 15:53:01 -0500

    change to Do not save

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Don't Save your changes instantly.</li>
+          <li>Do not Save your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
